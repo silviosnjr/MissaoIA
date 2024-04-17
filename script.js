e2d1d9e46256a7c1252b3f4e3ec8a869baadc6fc
silviosnjr <silviosnjr@gmail.com>
2024-04-17 18:06:54 -0300

    versão IA

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada){
 }
 
 function mostraResultado(){
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Em 2049....";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
