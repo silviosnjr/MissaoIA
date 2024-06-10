56a6d9ca19f6b21e6a77118245b8fb62f76f726f
silviosnjr <silviosnjr@gmail.com>
2024-06-09 22:05:30 -0300

    atualizando os grupos

---
+++ style_grupo3.css
@@ -0,0 +1,21 @@
+:root{
+    --cor-fundo: #01080E;
+    --cor-principal: #0B0D20 ;
+    --cor-secundaria: #212333;
+    --cor-destaque: #2BDEFD;
+    --cor-texto: #D7F9FF;
+}
+
+button{
+    background-color: var(--cor-secundaria);
+    color: var(--cor-texto);
+    border: none;
+    border-radius: 15px;
+    padding: 15px;
+    transition: background-color 0.3s;
+}
+
+button:hover{
+    background-color: var(--cor-destaque);
+    color: var(--cor-principal);
+}
